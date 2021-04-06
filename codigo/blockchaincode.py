5e7662ee319dd53e9e9cddd99ef2a5a2264c73b8
MrBarbo <ivan_barboza@outlook.com>
2021-04-05 22:54:38 -0300

    Prueba de Hash y Primer Bloque
    Prueba de Hash, usando una prueba de un bloque ejemplo
    
    Prueba de cadena[]

--- codigo/blockchaincode.py
+++ codigo/blockchaincode.py
@@ -1,12 +1,23 @@
 import json
 from datetime import datetime
 
-
-class Block:
-    def __init__(self, email, motiv, arch):
-        self.email = email
+#Crea el array de la cadena de bloques
+cadena  = []
+class Bloque:
+    def __init__(self, mail, motiv, arch):
+        self.email = mail
         self.motive = motiv 
         self.archivo = arch
         self.timestamp = datetime.now()
+        self.hash = hash(self)
+
+    def __hash__(self):
+        return hash((self.email, self.motive, self.archivo,self.timestamp))
+
+#Clase que maneja la cadena de bloques
+class BlockManager:
+    def __init__(self,cadena):
+        self.cadena = cadena
 
 
+cadena.append(Bloque("adios@gmail","mot2","files2.txt"))
--- tests/blockchaintest.py
+++ tests/blockchaintest.py
@@ -6,14 +6,24 @@ import unittest
 root_folder = os.path.abspath(os.path.dirname(os.path.dirname((__file__))))
 sys.path.append(root_folder)
 
-from codigo.blockchaincode  import Block
-
+from codigo.blockchaincode  import Bloque
+from codigo.blockchaincode import BlockManager
 class Test(unittest.TestCase):
-    def test_block(self):
-        Test = Block("hola@gmail.com","mot1","file1.txt")
-        self.assertEqual(email,"hola@gmail.com")
-        self.assertEqual(motive,"mot1")
-        self.assertEqual(archivo,"file1.txt")
+    def test_bloque(self):
+        Test = Bloque("hola@gmail.com","mot1","files1.txt")
+        self.assertEqual(Test.email,"hola@gmail.com")
+        self.assertEqual(Test.motive,"mot1")
+        self.assertEqual(Test.archivo,"files1.txt")
+        self.assertEqual(Test.hash,hash(Test))
+ 
+
+    def test_cadena(self):
+        cadenita = []
+        cadenita.append (Bloque("adios@gmail","mot2","files2.txt"))
+        Test = BlockManager(cadenita)
+        self.assertEqual(Test.cadena[0].hash, Bloque("adios@gmail","mot2","files2.txt").hash)
+
+
 
 if __name__ == "__main__":
     unittest.main()
