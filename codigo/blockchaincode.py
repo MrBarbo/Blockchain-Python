32dc22d40e13a70d58835a98ccc565380521c990
MrBarbo <ivan_barboza@outlook.com>
2021-04-10 00:26:37 -0300

    Serializacion
    - Serializacion de bloque
    -Bloque genesis
    - Funcion hash

--- codigo/blockchaincode.py
+++ codigo/blockchaincode.py
@@ -3,13 +3,17 @@ from datetime import datetime
 
 #Crea el array de la cadena de bloques
 cadena  = []
+
+
 class Bloque:
     def __init__(self, mail, motiv, arch):
         self.email = mail
         self.motive = motiv 
         self.archivo = arch
         self.timestamp = datetime.now()
-        self.hash = hash(self)
+        self.hashant = 0
+        hasher = mail + motiv + arch
+        self.hash = hash(hasher)
 
     def __hash__(self):
         return hash((self.email, self.motive, self.archivo,self.timestamp))
@@ -19,5 +23,20 @@ class BlockManager:
     def __init__(self,cadena):
         self.cadena = cadena
 
+#Metodo para crear bloque genesis
+    def __crear_bloque_genesis__(self):
+        bloqg = Bloque("","","")
+        self.cadena.append(bloqg)
 
-cadena.append(Bloque("adios@gmail","mot2","files2.txt"))
+
+#Método para ingresar un bloque
+    def __AgregarNuevo__(self, bloq):
+            ct = [Bloque]* (len(self.cadena)+1)#Crea el nuevo arreglo más grande
+            for i in range(len(self.cadena)):
+                ct[i]= self.cadena[i]
+            hant = self.cadena[len(self.cadena)-1].hash
+            bloq.hashant = hant
+            ct[len(self.cadena)]=bloq
+            self.cadena = ct
+
+
--- tests/blockchaintest.py
+++ tests/blockchaintest.py
@@ -8,20 +8,50 @@ sys.path.append(root_folder)
 
 from codigo.blockchaincode  import Bloque
 from codigo.blockchaincode import BlockManager
-class Test(unittest.TestCase):
-    def test_bloque(self):
+class ClassesTest(unittest.TestCase):
+    #(django project standard)
+    #Nombre de Tests: test_[Feature being tested]
+
+
+    #Test de la creacion de bloques
+    def test_BlockClassCreatesABlock(self):
         Test = Bloque("hola@gmail.com","mot1","files1.txt")
         self.assertEqual(Test.email,"hola@gmail.com")
         self.assertEqual(Test.motive,"mot1")
         self.assertEqual(Test.archivo,"files1.txt")
-        self.assertEqual(Test.hash,hash(Test))
- 
 
-    def test_cadena(self):
+    
+    #Test de la funcion hash
+    def test_BlockHashFunctionWorks(self):
+        Test = Bloque("Buendia@hola.com","mot6","file4.py")
+        self.assertEqual(Test.hash,hash("Buendia@hola.commot6file4.py"))
+
+    #Test de la funcion hash y creacion de la cadena
+    def test_HashFunctionAndBlockManagerWork(self):
         cadenita = []
         cadenita.append (Bloque("adios@gmail","mot2","files2.txt"))
         Test = BlockManager(cadenita)
         self.assertEqual(Test.cadena[0].hash, Bloque("adios@gmail","mot2","files2.txt").hash)
+
+
+    #Test de la creacion del bloque genesis
+    def test_BlockGenesisIsCreated(self):
+        cadenita=[]
+        Test = BlockManager(cadenita)
+        Test.__crear_bloque_genesis__()
+        self.assertEqual(Test.cadena[0].hash, hash(""))
+        
+
+    #Test de la funcion "AgregarBloque"    
+    def test_AgregarBloqueCreatesBlocks(self):
+        cadenita = []
+        Test = BlockManager(cadenita)
+        Test.__crear_bloque_genesis__()
+        b1 = Bloque("hola@out.com","mot1","file1.rar")
+        b2 = Bloque("adios@out.com","mot3","file2.zip")
+        Test.__AgregarNuevo__(b1)
+        Test.__AgregarNuevo__(b2)
+        self.assertEqual(Test.cadena[1].hash, Test.cadena[2].hashant)
 
 
 
