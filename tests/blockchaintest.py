6b64f6e3d07786c5f306ff83e3f333d5b1b0cf62
MrBarbo <ivan_barboza@outlook.com>
2021-04-21 19:37:37 -0300

    Versión Final
    - Todas las características requeridas obligatoriamente
    - Nombres y distribucion de archivos arregladas
    - Creación de archivo de hash
    - Arreglos varios

--- tests/blockchaintest.py
+++ tests/blockchaintest.py
@@ -8,66 +8,62 @@ sys.path.append(root_folder)
 
 from codigo.blockchaincode  import Bloque
 from codigo.blockchaincode import BlockManager
-class ClassTest(unittest.TestCase):
+class ChainTest(unittest.TestCase):
     #(django project standard)
     #Nombre de Tests: test_[Feature being tested]
-
-
-    #Test de la creacion de bloques
-    def test_BlockClassCreatesABlock(self):
-        Test = Bloque("hola@gmail.com","mot1","files1.txt")
-        self.assertEqual(Test.email,"hola@gmail.com")
-        self.assertEqual(Test.motive,"mot1")
-        self.assertEqual(Test.archivo,"files1.txt")
-
-    
-    #Test de la funcion hashing
-    def test_BlockHashFunctionWorks(self):
-        Test = Bloque("Buendia@hola.com","mot6","file4.py")        
-        self.assertEqual("0a0465b3bfdf37de3952f3919462f0d888c5c52ddfe06c4492199f7be9379df5",Test.hash)
-
-    #Test de que dos bloques iguales tienen hash iguales y creacion de una cadena
-    def test_HashFunctionAndBlockManagerWork(self):
-        b1 = Bloque("hola@out.com","mot1","file1.rar")
-        b2 = Bloque("hola@out.com","mot1","file1.rar")       
-        self.assertEqual(b1.hash,b2.hash)
-
-
+   
     #Test de la creacion del bloque genesis
     def test_BlockGenesisIsCreated(self):
         cadenita=[]
         Test = BlockManager(cadenita)
-        Test.__crear_bloque_genesis__()
-        self.assertEqual(Test.get_block(0).hashing(""),Test.get_block(0).hash)
+        Test._crear_bloque_genesis_()
+        bloq1 = Bloque("","","","")
+        self.assertEqual(bloq1.hash,Test.get_block(0).hash)
+        Test.remove_chain()
         
 
     #Test de la funcion "AgregarBloque", serializacion de bloques   
     def test_AgregarBloqueCreatesBlocks(self):
         cadenita = []
         Test = BlockManager(cadenita)
-        Test.__crear_bloque_genesis__()
+        Test._crear_bloque_genesis_()
         b1 = Bloque("hola@out.com","mot1","file1.rar")
         b2 = Bloque("adios@out.com","mot3","file2.zip")
         Test.agregar_nuevo(b1)
         Test.agregar_nuevo(b2)
         self.assertEqual(Test.get_block(1).hash, Test.get_block(2).hashant)
+        Test.remove_chain()
+
+    #Test patron singleton
+    def test_Singleton(self):
+        cadenita = []
+        test1 = BlockManager(cadenita)
+        test2 = BlockManager(cadenita)
+        test1._crear_bloque_genesis_()
+        b1 = Bloque("hola@out.com","mot1","file1.rar")
+        test1.agregar_nuevo(b1)
+        self.assertEqual(b1.hash, test2.get_block(1).hash)
+        test1.remove_chain()
+
+
 
     #Test de la busqueda por indice (get_block)
     def test_IndexedSearchWorks(self):
         cadenita = []
         Test = BlockManager(cadenita)
-        Test.__crear_bloque_genesis__()
+        Test._crear_bloque_genesis_()
         b1 = Bloque("buendia@in.com","mot65","file7.py")
         b2 = Bloque("adios@out.com","mot3","file2.zip")
         Test.agregar_nuevo(b1)
         Test.agregar_nuevo(b2)
-        self.assertEqual(Test.get_block(2).email,"adios@out.com")
+        self.assertEqual("adios@out.com", Test.get_block(2).email)
+        Test.remove_chain()
 
     #Test de la busqueda por hash
     def test_HashSearchWorks(self):
         cadenita = []
         Test = BlockManager(cadenita)
-        Test.__crear_bloque_genesis__()
+        Test._crear_bloque_genesis_()
         b1 = Bloque("buendia@in.com","mot65","file7.py")
         b2 = Bloque("buenastardes@jd.com","mot8","arch1.zip")
         Test.agregar_nuevo(b1)
@@ -75,21 +71,37 @@ class ClassTest(unittest.TestCase):
         bt = Bloque("buendia@in.com","mot65","file7.py")
         ht = bt.hash
         self.assertEqual(Test.busqueda_hash(ht).hash,ht)
+        Test.remove_chain()
 
     #Calculando hashes bloque por bloque verificamos la cadena
     def test_ChainVerification(self):
         cadenita = []
-        Test = BlockManager(cadenita)
-        Test.__crear_bloque_genesis__()
-        b1 = Bloque("buendia@in.com","mot65","file7.py")
-        b2 = Bloque("buenastardes@jd.com","mot8","arch1.zip")
-        b3 = Bloque("adios@uy.com", "mot20","arch4.mp3")
-        Test.agregar_nuevo(b1)
-        Test.agregar_nuevo(b2)
-        Test.agregar_nuevo(b3)
-        self.assertEqual(Test.get_block(1).hashant,Test.get_block(0).hash)
-        self.assertEqual(Test.get_block(2).hashant, Test.get_block(1).hash)
-        self.assertEqual(Test.get_block(3).hashant,Test.get_block(2).hash)
+        test = BlockManager(cadenita)
+        test._crear_bloque_genesis_()
+
+        for m in range(1,101):
+            btest = Bloque(str(m) + "ia@gmail.com","mot65","file3.exe","2021-04-19 20:48")
+            test.agregar_nuevo(btest)
+
+        for j in range(1,101):
+            self.assertEqual(test.get_block(j).hashant,test.get_block(j-1).hash)
+            btest = Bloque(str(j) + "ia@gmail.com","mot65","file3.exe","2021-04-19 20:48")
+            self.assertEqual(btest.hash,test.get_block(j).hash)
+        test.remove_chain()
+
+    #Prueba que el archivo de los hash funcione correctamente        
+    def test_HashFile(self):
+        cadenita = []
+        test = BlockManager(cadenita)
+        test._crear_bloque_genesis_()
+        btest = Bloque("uioo@gmail.com","mot25","file5.exe","2021-04-19 20:48")
+        test.agregar_nuevo(btest)
+        test.hashes_to_file()
+        file1 = open("file.txt","r")
+        file1.seek(0)
+        self.assertEqual(test.cadena[0].hash+test.cadena[1].hash, file1.readline())
+        file1.close()
+        test.remove_chain()
 
 if __name__ == "__main__":
     unittest.main()
